2824cd4532d2c56f66913f6d6bd768263064348e
GerA <lpgera@gmail.com>
2026-03-08 14:06:44 +0100

    init repo

---
+++ README.md
@@ -0,0 +1,4 @@
+# esp32-c6-matter-temperature-sensor
+
+A Matter-enabled ESP32-C6 sensor that measures temperature, humidity, and pressure using a BME280, and exposes the data to smart home ecosystems.
+
---
+++ src/main.cpp
@@ -0,0 +1,90 @@
+#include <Arduino.h>
+#include <Matter.h>
+#include <Adafruit_BME280.h>
+#include <Ticker.h>
+
+Adafruit_BME280 bme;
+Adafruit_Sensor *bme_temp = bme.getTemperatureSensor();
+Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
+Adafruit_Sensor *bme_humidity = bme.getHumiditySensor();
+sensors_event_t temperatureEvent, humidityEvent, pressureEvent;
+
+MatterTemperatureSensor temperatureSensor;
+MatterHumiditySensor humiditySensor;
+MatterPressureSensor pressureSensor;
+uint32_t buttonTimestamp = 0;
+bool buttonState = false;
+
+bool commissioningState = true;
+bool ledState = false;
+void toggleLED() {
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, ledState);
+}
+Ticker ledTicker(toggleLED, 500);
+
+void getLatestMeasurements() {
+    bme_temp->getEvent(&temperatureEvent);
+    bme_pressure->getEvent(&pressureEvent);
+    bme_humidity->getEvent(&humidityEvent);
+
+    Serial.printf("Temperature: %f C, Humidity: %f %%, Pressure: %f hPa \n", temperatureEvent.temperature, humidityEvent.relative_humidity, pressureEvent.pressure);
+
+    temperatureSensor.setTemperature(temperatureEvent.temperature);
+    humiditySensor.setHumidity(humidityEvent.relative_humidity);
+    pressureSensor.setPressure(pressureEvent.pressure);
+}
+Ticker measurementTicker(getLatestMeasurements, 30000);
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(BOOT_PIN, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
+    bme.begin(0x76);
+
+    bme_temp->getEvent(&temperatureEvent);
+    bme_pressure->getEvent(&pressureEvent);
+    bme_humidity->getEvent(&humidityEvent);
+
+    temperatureSensor.begin(temperatureEvent.temperature);
+    humiditySensor.begin(humidityEvent.relative_humidity);
+    pressureSensor.begin(pressureEvent.pressure);
+    ArduinoMatter::begin();
+
+    measurementTicker.start();
+
+    if (!ArduinoMatter::isDeviceCommissioned()) {
+        Serial.println("Device is not commissioned. Waiting for setup...");
+        Serial.print("Manual pairing code: ");
+        Serial.println(ArduinoMatter::getManualPairingCode());
+        Serial.print("QR code URL: ");
+        Serial.println(ArduinoMatter::getOnboardingQRCodeUrl());
+    } else {
+        Serial.println("Device is already commissioned.");
+    }
+}
+
+void loop() {
+    ledTicker.update();
+    measurementTicker.update();
+
+    if (ArduinoMatter::isDeviceCommissioned() != commissioningState) {
+        commissioningState = ArduinoMatter::isDeviceCommissioned();
+        if (commissioningState) {
+            ledTicker.stop();
+            digitalWrite(LED_BUILTIN, HIGH);
+        } else {
+            ledTicker.start();
+        }
+    }
+
+    if (digitalRead(BOOT_PIN) == LOW && !buttonState) {
+        buttonTimestamp = millis();
+        buttonState = true;
+    }
+    if (buttonState && millis() - buttonTimestamp > 5000) {
+        Serial.println("Decommissioning...");
+        ArduinoMatter::decommission();
+        buttonTimestamp = millis();
+    }
+}
